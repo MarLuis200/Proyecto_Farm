b62fb35007568485089e6f234e7dfa55bfc18b19
ManuelG0408 <L202207045@vbravo.tecnm.mx>
2024-06-04 13:00:30 -0600

    changes.manuel

--- resources/views/admin/inventario/index.blade.php
+++ resources/views/admin/inventario/index.blade.php
@@ -27,8 +27,8 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-5">
+                                <!-- Logo -->
                                 <div class="logo">
-                                    <br><br>
                                     <h1>Inventario</h1>
                                 </div>
                             </div>
@@ -36,7 +36,7 @@
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <div class="input-group form">
-                                            <!--
+                                            <!-- 
                                             <input type="text" class="form-control" placeholder="Buscar...">
                                             <span class="input-group-btn">
                                                 <button class="btn btn-primary" type="button">Buscar</button>
@@ -48,11 +48,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="navbar navbar-inverse" role="banner">
-                                    <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-                                        <ul class="nav navbar-nav">
-                                            <li><a href="{{ route('admin/inventario') }}">Inventario</a></li>
-                                        </ul>
-                                    </nav>
+                                    <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation"></nav>
                                 </div>
                             </div>
                         </div>
