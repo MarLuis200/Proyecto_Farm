aff70dae15a3a3ad6e3547c1c8b56935e943b282
ManuelG0408 <L202207045@vbravo.tecnm.mx>
2024-06-17 10:11:42 -0600

    changes.manuel

--- resources/views/salud.blade.php
+++ resources/views/salud.blade.php
@@ -131,9 +131,99 @@
                         </section>
 
                         <h2>Encuentra las categorias de Salud</h2>
+                
+                        <div class="container mt-5">
+                            
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <!-- Primera tanda de imágenes -->
+                                <div class="carousel-item active">
+                                    <div class="d-flex justify-content-center">
+                                        <div>
+                                            <a class="nav-link" href="/salud/antibioticos"><img src="https://www.movil.farmaciasguadalajara.com/wcsstore/FGCAS//wcs/images/content/CAT_THUMB/medicina/ANTIBIOTICOS/DESK_2X_MEDICINA_ANTIBIOTICO.png" class="d-block mx-3" alt="Imagen 1"></a>
+                                            <div class="container">
+                                                <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Antibioticos</a>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <img src="https://www.movil.farmaciasguadalajara.com/wcsstore/FGCAS//wcs/images/content/CAT_THUMB/medicina/PRIMEROS%20AUX/DESK_2X_MEDICINA_PRIMEROS%20AUXILIOS.png" class="d-block mx-3" alt="Imagen 2">
+                                            <div class="container">
+                                                <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Dolor</a>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <img src="https://www.movil.farmaciasguadalajara.com/wcsstore/FGCAS//wcs/images/content/site/banners_circulares/D2D5C3/D2D5C3.png" class="d-block mx-3" alt="Imagen 3">
+                                            <div class="container">
+                                                <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Dermatologia</a>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <img src="/img/saludsexual.jpg" class="d-block mx-3" alt="Imagen 4">
+                                            <div class="container">
+                                                <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Salud Sexual</a>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <img src="/img/formulas.jpg" class="d-block mx-3" alt="Imagen 5">
+                                            <div class="container">
+                                                <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Formulas Infantiles</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Segunda tanda de imágenes -->
+                                <div class="carousel-item">
+                                    <div class="d-flex justify-content-center">
+                                            <div>
+                                                <img src="/img/multivitaminas.jpg" class="d-block mx-3" alt="Imagen 1">
+                                                <div class="container">
+                                                    <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Multivitaminas</a>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <img src="/img/alimentos.jpg" class="d-block mx-3" alt="Imagen 2">
+                                                <div class="container">
+                                                    <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Alimentos</a>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <img src="/img/bebidas.jpg" class="d-block mx-3" alt="Imagen 3">
+                                                <div class="container">
+                                                    <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Bebidas</a>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <img src="/img/higiene.jpg" class="d-block mx-3" alt="Imagen 4">
+                                                <div class="container">
+                                                    <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Higiene y Belleza</a>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <img src="/img/pañales.jpg" class="d-block mx-3" alt="Imagen 5">
+                                                <div class="container">
+                                                    <a tabindex="-1" aria-hidden="true" class="product_group_name product_info" href="">Pañales</a>
+                                                </div>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Anterior</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Siguiente</span>
+                            </a>
+                            </div>
+                        </div>
 
-                                         
-                    
+                                        
+                        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+                        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
                         
 
-                        
+
