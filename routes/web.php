33aee9fdba508feb42f5fff11bbda2742eb261d2
Luis Martinez <tony7004mar@gmail.com>
2024-06-23 10:25:30 -0600

    Changues Luis

--- routes/web.php
+++ routes/web.php
@@ -153,6 +153,9 @@ Route::get('/salud', 'App\Http\Controllers\SaludController@index')->name('/salud
 Route::get('/salud/antibioticos', 'App\Http\Controllers\AntibioticosController@index')->name('/salud/antibioticos');
 
 Route::get('/dash2', [Dash2Controller::class, 'index'])->name('dash2')->middleware('auth');
+Route::get('/salud/dolor', 'App\Http\Controllers\DolorController@index')->name('/salud/dolor');
+
+Route::get('/dash2', [Dash2Controller::class, 'index'])->name('dash2')->middleware('auth');
 
 // routes/web.php
 Route::resource('ventas', VentaController::class);
